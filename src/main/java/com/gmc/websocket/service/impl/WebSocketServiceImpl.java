03c3a27c03c655d3c561dc411ece2bbc0d25ecbb
hailey <wjdguswls55@gmail.com>
2021-11-15 16:07:04 +0900

    feat : 웹소켓 연결 예외처리 로그 추가

--- src/main/java/com/gmc/websocket/service/impl/WebSocketServiceImpl.java
+++ src/main/java/com/gmc/websocket/service/impl/WebSocketServiceImpl.java
@@ -3,6 +3,7 @@ package com.gmc.websocket.service.impl;
 import com.gmc.websocket.okex.OkexWebSocketClientEndpoint;
 import com.gmc.websocket.repository.WebSocketOrderbookRepository;
 import com.gmc.websocket.service.WebSocketService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Service;
 
@@ -12,6 +13,7 @@ import javax.websocket.WebSocketContainer;
 import java.net.URI;
 
 @Service
+@Slf4j
 public class WebSocketServiceImpl implements WebSocketService {
 
     private final WebSocketOrderbookRepository webSocketOrderbookRepository;
@@ -30,6 +32,9 @@ public class WebSocketServiceImpl implements WebSocketService {
             final Session session = container.connectToServer(OkexWebSocketClientEndpoint.class, URI.create(uri));
             return session;
         } catch (Exception e) {
+            log.info("okexWebSocket exceptions");
+            log.info(String.valueOf(e));
+
             throw new RuntimeException(e);
         }
     }
